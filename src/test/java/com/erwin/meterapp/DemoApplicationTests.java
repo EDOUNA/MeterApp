0e11198ae7eb9abaf72af74ea1743233309b4bdd
Erwin Douna <e.douna@gmail.com>
2020-02-12 13:44:40 +0100

    Initial commit

---
+++ src/test/java/com/erwin/meterapp/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erwin.meterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
