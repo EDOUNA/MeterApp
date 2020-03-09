1387984e2496e7aedb2bec4e7f28ea2d6cf9cee7
Erwin Douna <e.douna@gmail.com>
2020-03-09 22:16:02 +0100

    Adding currencies Renaming to proper model

---
+++ src/main/java/com/erwin/meterapp/persistence/model/CurrenciesModel.java
@@ -0,0 +1,104 @@
+package com.erwin.meterapp.persistence.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "currencies")
+public class CurrenciesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private Timestamp updatedAt;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "symbol")
+    private String symbol;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "active")
+    private int active;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrenciesModel{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                ", description='" + description + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", code='" + code + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
--- src/main/java/com/erwin/meterapp/persistence/model/DeviceTariffsModel.java
+++ src/main/java/com/erwin/meterapp/persistence/model/DeviceTariffsModel.java
@@ -2,13 +2,15 @@ package com.erwin.meterapp.persistence.model;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "device_tariffs")
-public class DeviceTariffs {
+public class DeviceTariffsModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -26,4 +28,11 @@ public class DeviceTariffs {
 
     @Column(name = "amount")
     private float amount;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    @Nullable
+    private LocalDate endDate;
 }
